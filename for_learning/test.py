d7bab92426b97de787ad392f6d1486878326d650
lxwcu <43214555+lxwcu@users.noreply.github.com>
2019-10-05 22:27:44 +0800

    exercise 3

--- for_learning/test.py
+++ for_learning/test.py
@@ -25,13 +25,20 @@
 # mailCheck(mailDa)
 
 #exercise2
-def prArr(userInfo):
-    for item in userInfo:
-        print("用户：%8s， 积分：%7.1f， 等级：%2s， 头衔：%s" %tuple(item))
+# def prArr(userInfo):
+#     for item in userInfo:
+#         print("用户：%8s， 积分：%8.2f， 等级：%2s， 头衔：%s" %tuple(item))
+#
+# info = [
+#     ['user1', 345.6, 12, '黄金'],
+#     ['user2', 2345.6, 8, '白银'],
+#     ['user3555', 55345.6, 22, '钻石'],
+# ]
+# prArr(info)
 
-info = [
-    ['user1', 345.6, 12, '黄金'],
-    ['user2', 2345.6, 8, '白银'],
-    ['user3555', 55345.6, 22, '钻石'],
-]
-prArr(info)
+#exercise3
+def getNameFromStr(strs):
+    nameList = strs.split("\n")
+    return nameList
+
+def 
